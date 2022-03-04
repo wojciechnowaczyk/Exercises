f6ea952117cab1d0ce3a6d10d0048024afd2c04e
Wojtek Nowaczyk <wojciech.nowaczyk@espo.eu>
2022-03-04 14:50:44 +0100

    File api exercises

--- File/script.js
+++ File/script.js
@@ -1,9 +1,47 @@
 window.onload = function () {
-  var fileInput = document.getElementById("fileInput");
-  fileInput.addEventListener("onchange", readFiles);
-  function readFiles(event) {
-    console.log(event);
+  //reading files
+  var multipleFilesInput = document.getElementById("multipleFilesInput");
+  multipleFilesInput.addEventListener("change", getFiles, false);
+  function getFiles() {
+    var files = this.files;
+    console.log(files);
   }
+
+  //file reader
+  var fileInput = document.getElementById("fileInput");
+  fileInput.addEventListener("change", getFiles, false);
+  function getFiles() {
+    var file = this.files[0];
+    console.log(file);
+    //load as text
+    var reader = new FileReader();
+    reader.onload = function (loadedEvent) {
+      console.log(loadedEvent.target.result);
+    };
+    // reader.readAsText(file);
+
+    //monitoring progress of file loading
+    var reader = new FileReader();
+
+    reader.onprogress = function (progressEvent) {
+      console.log("onprogress");
+      if (progressEvent.lengthComputable) {
+        var percentLoaded = Math.round(
+          (progressEvent.loaded * 100) / progressEvent.total
+        );
+      }
+      console.log(
+        "total: " +
+          progressEvent.total +
+          ", loaded: " +
+          progressEvent.loaded +
+          "(" +
+          percentLoaded +
+          "%)"
+      );
+    };
+  }
+  //drag & drop file api
   function drop(evt) {
     evt.stopPropagation();
     evt.preventDefault();
@@ -15,8 +53,6 @@ window.onload = function () {
       fileEl.innerHTML = file.name;
       dropZone.appendChild(fileEl);
     }
-
-    // access files via fileList
   }
 
   function dragOver(evt) {
